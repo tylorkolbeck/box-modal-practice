0086ec87a1ca4bfc31b30d664972c3e424913034
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-05 14:25:58 -0800

    fixed issue with running event listener function not updating all the styles

--- logic.js
+++ logic.js
@@ -1,38 +1,22 @@
-
-
-
 let boxModal = document.getElementById("box-modal-wrapper")
 let paddingInput = document.getElementById("padding-input")
 let marginInput = document.getElementById("margin-input")
 let borderInput = document.getElementById("border-input")
 
-
 // Styles 
 let margin = window.getComputedStyle(boxModal).margin
 let padding = window.getComputedStyle(boxModal).padding
-let border = window.getComputedStyle(boxModal).border
+let border = window.getComputedStyle(boxModal).borderWidth
 
-// Set input values
-paddingInput.value = padding
-marginInput.value = margin
-borderInput.value = border.split(' ')[0]
-
-// Event Listener Functions
-function updatePadding() {
-    boxModal.setAttribute("style", `padding: ${paddingInput.value};`)
+function updateStyling() {
+    boxModal.style.margin = marginInput.value
+    boxModal.style.borderWidth = borderInput.value
+    boxModal.style.padding = paddingInput.value
 }
 
-function updateMargin() {
-    boxModal.setAttribute("style", `margin: ${marginInput.value};`)
-}
-
-function updateBorder() {
-    boxModal.setAttribute("style", `border-width: ${borderInput.value};`)
-}
-
-paddingInput.addEventListener("input", updatePadding, true)
-marginInput.addEventListener("input", updateMargin, true)
-borderInput.addEventListener("input", updateBorder, true)
+paddingInput.addEventListener("input", updateStyling)
+marginInput.addEventListener("input", updateStyling)
+borderInput.addEventListener("input", updateStyling)
 
 
 
