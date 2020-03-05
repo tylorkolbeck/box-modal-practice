f995b510f13b2f38b14f7d26fe43e947a8ebffc8
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-05 14:38:00 -0800

    refactor

--- logic.js
+++ logic.js
@@ -1,22 +1,23 @@
 let boxModal = document.getElementById("box-modal-wrapper")
-let paddingInput = document.getElementById("padding-input")
-let marginInput = document.getElementById("margin-input")
-let borderInput = document.getElementById("border-input")
 
-// Styles 
-let margin = window.getComputedStyle(boxModal).margin
-let padding = window.getComputedStyle(boxModal).padding
-let border = window.getComputedStyle(boxModal).borderWidth
+let inputs = [
+    document.getElementById("padding-input"),
+    document.getElementById("margin-input"),
+    document.getElementById("border-input")
+]
+
+inputs.forEach(input => {
+    input.addEventListener("input", updateStyling)
+})
 
 function updateStyling() {
-    boxModal.style.margin = marginInput.value
-    boxModal.style.borderWidth = borderInput.value
-    boxModal.style.padding = paddingInput.value
+    boxModal.style.margin = inputs[1].value
+    boxModal.style.borderWidth = inputs[2].value
+    boxModal.style.padding = inputs[0].value
 }
 
-paddingInput.addEventListener("input", updateStyling)
-marginInput.addEventListener("input", updateStyling)
-borderInput.addEventListener("input", updateStyling)
+
+
 
 
 
